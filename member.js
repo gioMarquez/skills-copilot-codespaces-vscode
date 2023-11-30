5a89ecb30b733a23ecd33d9272473df07c45bf3f
gio_fmz <jhovannyfragoso@outlook.com>
2023-11-30 01:52:14 -0600

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var skillsMember = document.getElementById("skillsMember");
+  var projectsMember = document.getElementById("projectsMember");
+  var contactMember = document.getElementById("contactMember");
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+  skillsMember.style.display = "none";
+  projectsMember.style.display = "none";
+  contactMember.style.display = "none";
+}
